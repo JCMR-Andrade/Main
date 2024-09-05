7783afe6d81567c73a74c4009600696254b3ce24
JCMR-Andrade <152985932+JCMR-Andrade@users.noreply.github.com>
2024-09-05 13:59:13 -0300

    Update Extração UOL HELIUM_VF_(05.09.24).py

--- Web Scraping/Extração UOL HELIUM_VF_05.09.24).py
+++ Web Scraping/Extração UOL HELIUM_VF_05.09.24).py
@@ -1,18 +1,16 @@
-from helium import start_chrome, find_all, S
+from helium import *
 
-# Inicia o navegador e navega até o site do UOL
-drive = start_chrome('https://pt.dotabuff.com/players/36423302/matches?enhance=overview', headless=True)
-
+drive = helium.start_chrome('uol.com.br', headless=True)
 
 # Encontra todos os elementos que contêm os links e títulos das matérias
-email_cells = find_all(S("table > tr > td", below="section"))
-emails = [cell.web_element.text for cell in email_cells]
+articles = find_all(S('a'))
 
-print(emails)
+# Filtra e exibe os links e títulos
+for article in articles:
+    title = article.web_element.text
+    link = article.web_element.get_attribute('href')
+    if title and link:
+        print(f'Título: {title}\nLink: {link}\n')
 
-# # Filtra e exibe os links e títulos
-# for article in articles:
-#     title = article.web_element.text
-#     link = article.web_element.get_attribute('href')
-#     if title and link:
-#         print(f'Título: {title}\nLink: {link}\n')
+
+kill_browser()
